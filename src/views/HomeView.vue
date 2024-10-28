0c8c5b97a3cdf6b83444b016bf7c0fae73f5e6c7
Qi-Mo Lin <qimolin9@gmail.com>
2024-10-28 19:40:39 +0100

    Refactor home view component to display job titles with typewriter effect using css only

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <template>
   <main>
     <section class="first-section">
-      <article class="first-section-article">
-        <h1>I'm Qi Mo Lin</h1>
-        <h2 class="description typewriter-effect" aria-label="Fullstack Developer"></h2>
+      <article>
+        <h1 class="content">
+          <span>I am</span> Qi Mo Lin
+          <ol>
+            <li v-for="jobTitle in jobTitles" :key="jobTitle">
+              <span>{{ jobTitle }}</span>
+            </li>
+          </ol>
+        </h1>
         <hr class="divider" />
       </article>
       <img class="qimo-img" src="@/assets/qi_mo_lin.webp" />
@@ -12,83 +18,89 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-
-const phrases = ["Software Developer"];
-let firstLoop = 0;
-let secondLoop = 0;
-const currentPhrase: string[] = [];
-let isDeleting = false;
-
-function loopTypeWriter() {
-  const h2Text = document.querySelector(".typewriter-effect");
-  if (firstLoop < phrases.length) {
-    if (!h2Text) return;
-    if (!isDeleting && secondLoop <= phrases[firstLoop].length) {
-      currentPhrase.push(phrases[firstLoop][secondLoop]);
-      secondLoop++;
-      h2Text.innerHTML = currentPhrase.join("");
-    }
-
-    if (isDeleting && secondLoop <= phrases[firstLoop].length) {
-      currentPhrase.pop();
-      secondLoop--;
-      h2Text.innerHTML = currentPhrase.join("");
-    }
-
-    if (secondLoop === phrases[firstLoop].length) {
-      isDeleting = true;
-    }
-
-    if (isDeleting && secondLoop === 0) {
-      isDeleting = false;
-      firstLoop++;
-    }
-  } else firstLoop = 0;
-  const deletingSpeed = 50;
-  const normalSpeed = 200;
-  const time = isDeleting ? deletingSpeed : normalSpeed;
-  setTimeout(loopTypeWriter, time);
-}
-
-onMounted(() => {
-  loopTypeWriter();
-});
+const jobTitles = [
+  "Software Developer",
+  "Web Developer",
+  "Frontend Developer",
+  "Backend Developer",
+  "Fullstack Developer",
+  "MSc Student",
+];
 </script>
+
+<style>
+:root {
+  --job-count: 6;
+  --animation-delay: 3s;
+}
+</style>
 
 <style scoped>
 .first-section {
   height: calc(100vh - 20rem);
-}
-.first-section-article > .description {
-  font-size: var(--step-2);
-  text-align: center;
-  margin-bottom: 12px;
-  color: var(--color-primary);
-}
-.first-section-article > .description::after {
-  content: "";
-  border-right: 0.15em solid;
-  animation: blinking 1.2s step-end infinite;
-}
-@keyframes blinking {
-  from,
-  to {
-    border-color: transparent;
-  }
-  50% {
-    border-color: var(--color-primary);
-  }
 }
 .qimo-img {
   width: min(65%, 350px);
   border-radius: 50%;
   border: solid 3px var(--color-primary);
 }
-@media (min-width: 1025px) {
-  .first-section-article > .description {
-    text-align: start;
-    margin-bottom: 20px;
+.first-section-article h1 > span {
+  color: var(--color-primary);
+}
+.content {
+  text-align: left;
+  letter-spacing: 0.15rem;
+}
+.content span {
+  color: var(--color-primary);
+}
+.content ol li {
+  animation: slide calc(var(--job-count) * var(--animation-delay) * 2)
+    steps(var(--job-count)) infinite;
+}
+@keyframes slide {
+  100% {
+    transform: translateY(calc(var(--job-count) * -100%));
+  }
+}
+.content ol {
+  list-style: none;
+  height: 3.5rem;
+  line-height: 3.5rem;
+  overflow: hidden;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+}
+.content ol li span {
+  font-weight: 600;
+  -webkit-text-fill-color: transparent;
+  background-color: var(--color-primary);
+  -webkit-background-clip: text;
+  background-clip: text;
+  white-space: nowrap;
+
+  border-right: 0.4rem solid var(--color-primary);
+
+  display: inline-block;
+  width: 0%;
+  animation: cursor 0.8s step-end infinite,
+    typing var(--animation-delay) steps(18) infinite alternate;
+}
+
+@keyframes cursor {
+  50% {
+    border-color: transparent;
+  }
+}
+@keyframes typing {
+  0%,
+  10% {
+    width: -1%;
+  }
+  70%,
+  100% {
+    width: 100%;
   }
 }
 </style>
