5978a5823ffcbfd5fa7845cabd8e5a4db20b85a0
Qi-Mo Lin <qimolin9@gmail.com>
2024-10-29 22:27:03 +0100

    Add rollup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer() as PluginOption,
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
